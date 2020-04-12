4c28721cd6deb05857ba8136ee3eab12c9819b53
AdamDWalker <adamwalker03@googlemail.com>
2020-04-12 14:58:21 +0100

    bloodhound.sh can now read from stdin

--- bloodhound.sh
+++ bloodhound.sh
@@ -3,23 +3,30 @@
 # Bloodhound
 # Simple script to sniff through JS files and find interesting things. For now it will probably just be really basic
 
-targets=("token" "key" "api" "amazonaws" "username" "password" "pass" "credential" "secret" "todo" "vulnerable" "random" "hmac" "md5" "sha-1" "sha-2" "hash" "csrf" "hostname" "window")
+targets=("token" "key" "api" "amazonaws" "username" "password" "pass" "credential" "secret" "todo" "vulnerable" "random" "hmac" "md5" "sha-1" "sha-2" "hash" "csrf" "hostname" "auth")
 
-echo ""
-echo "Downloading JS file..."
-echo ""
 
-wget $1 -O input.js
+while read LINE; do
+   
+	echo "==============="
+	echo ${LINE}    # do something with it here
 
-echo ""
-echo "Beginning scan..."
-echo ""
+	echo ""
+	echo "Downloading JS file..."
+	echo ""
 
-for i in "${targets[@]}"
-do
-	grep -in $i input.js --color
+	wget ${LINE} -O input.js
+
+	echo ""
+	echo "Beginning scan..."
+	echo ""
+
+	for i in "${targets[@]}"
+	do
+		grep -in $i input.js --color
+	done
+
+	echo ""
+	echo "Scan complete."
+	echo ""
 done
-
-echo ""
-echo "Scan complete."
-echo ""
