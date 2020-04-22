fc135b2d867ce12c892ec7e6514e8899bac0a3c9
AdamDWalker <adamwalker03@googlemail.com>
2020-04-22 13:04:14 +0100

    Added JS beautifier support to bloodhound
    - Script now downloads file and the beautifys it before scanning. This
    should produce a clearer output
    - Adjusted some file names slightly

--- bloodhound.sh
+++ bloodhound.sh
@@ -3,11 +3,16 @@
 # Bloodhound
 # Simple script to sniff through JS files and find interesting things. For now it will probably just be really basic
 
-targets=("token" "key" "api" "amazonaws" "username" "password" "pass" "credential" "secret" "todo" "vulnerable" "random" "hmac" "md5" "sha-1" "sha-2" "hash" "csrf" "hostname" "auth")
 
+#
+# TODO: Make the target list into an external wordlist and implement argument parsing 
+#
+
+targets=("token" "key" "api" "amazonaws" "username" "password" "pass" "credential" "secret" "todo" "vulnerable" "random" "hmac" "md5" "sha-1" "sha-2" "hash" "csrf" "hostname" "auth")
+inputfile=input.js
 
 while read LINE; do
-   
+
 	echo "==============="
 	echo ${LINE}    # do something with it here
 
@@ -15,15 +20,21 @@ while read LINE; do
 	echo "Downloading JS file..."
 	echo ""
 
-	wget ${LINE} -O input.js
+	wget ${LINE} -O $inputfile
 
+	echo ""         
+	echo "Beautifying JS file..."
+	echo ""
+
+	js-beautify $inputfile >> target.js
+	
 	echo ""
 	echo "Beginning scan..."
 	echo ""
 
 	for i in "${targets[@]}"
 	do
-		grep -in $i input.js --color
+		grep -in $i target.js --color
 	done
 
 	echo ""
