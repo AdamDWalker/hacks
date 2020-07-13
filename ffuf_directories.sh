9103ed3ada445528c4ee99cce8388085653ee52b
AdamDWalker <adamwalker03@googlemail.com>
2020-07-13 23:00:25 +0100

    Script to automate directory scanning with ffuf
    - I suck at ffuf so I used this to learn it a little better and to make
    it easier to use without having to pay attention to the results
    - It takes a list of subdomains, directory scans them and saves the
    results to a formatted txt file

---
+++ ffuf_directories.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+while IFS= read -r line
+do
+
+ffuf -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$line/FUZZ" -w ~/Documents/Git/wordlists/dicc.txt -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -o result_dir.tmp
+
+
+cat result_dir.tmp | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' >> directories.txt
+
+rm result_dir.tmp
+
+done < $1
