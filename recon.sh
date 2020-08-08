823bcaa38a5c5bf00121a5273a3f265e731ac1ea
AdamDWalker <adamwalker03@googlemail.com>
2020-08-08 16:29:44 +0100

    Updates from ages ago that I forgot to commit

--- recon.sh
+++ recon.sh
@@ -2,8 +2,8 @@
 
 ## This requires the aliases setup from here - https://edoverflow.com/2017/lightweight-reconnaissance-setup/
 
-mkdir recon 
-cd recon/
+mkdir $1 
+cd $1/
 
 ## Sublist3r -- Get subdomains
 python ~/tools/Sublist3r/sublist3r.py -d $1 -o output
@@ -15,7 +15,7 @@ cat output | httprobe | tee -a domains
 python ~/tools/webscreenshot/webscreenshot.py -i domains
 
 ## Dirsearch-one command converted to run with full filepaths and no backslashes
-. <(cat domains | awk '{print "python3 ~/tools/dirsearch/dirsearch.py -u " $1 " -e * --json-report=directories"}')
+. <(cat domains | awk '{print "python3 ~/tools/dirsearch/dirsearch.py -u " $1 " -e * --plain-text-report=directories_$1"}')
 
 ## Openredirect
-. <(cat domains | awk '{print "python3 ~/tools/dirsearch/dirsearch.py -u " $1  " -w ~/tools/dirsearch/db/open_redirect_wordlist.txt -e * --json-report=redirects"}')
+. <(cat domains | awk '{print "python3 ~/tools/dirsearch/dirsearch.py -u " $1  " -w ~/tools/dirsearch/db/open_redirect_wordlist.txt -e * --plain-text-report=redirects_$1"}')
