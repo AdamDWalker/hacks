a250884e26173dda526c099a1ede265df25e56d6
AdamDWalker <adamwalker03@googlemail.com>
2020-09-09 11:00:49 +0100

    Lots of new code for artemis
    - Wrote the code ages ago so I don't remember what stuff I added

--- artemis.sh
+++ artemis.sh
@@ -39,6 +39,7 @@ rm $domain/sublist3r_subs.txt
 echo ""
 echo $'\e[32m'"Finding Live Subdomains..." $'\e[0m'
 cat $domain/subdomains.txt | httprobe -c 50 -t 3000 > $domain/live_subdomains.txt
+sort -u live_subdomains.txt
 
 echo ""
 echo $'\e[1;32m'"----- Subdomain Search Completed -----" $'\e[0m'
@@ -52,22 +53,80 @@ echo $'\e[1;33m'"======== Aquatone Started ========" $'\e[0m'
 
 cat $domain/live_subdomains.txt | aquatone -ports xlarge -out $domain/aquatone/
 
+echo ""
+echo ""
+echo $'\e[1;33m'"======== Gathering Files ========" $'\e[0m'
+
+if curl --output ${domain}/robots.txt "https://${domain}/robots.txt"; then
+  echo "File downloaded: https://${domain}/robots.txt"
+else
+  echo "https://${domain}/robots.txt does not seem to exist."
+fi
+
+echo ""
+echo $'\e[32m'"Finding Links..." $'\e[0m'
+
+while IFS= read -r line; do
+
+	echo "" | tee -a  ${domain}/links.txt
+	echo "----- $line -----" | tee -a ${domain}/links.txt
+	python3 ~/tools/LinkFinder/linkfinder.py -i $line -o cli | tee -a ${domain}/links.txt
+
+done < $domain/live_subdomains.txt
 
 
+echo ""
+echo $'\e[32m'"Finding Secrets..." $'\e[0m'
 
+while IFS= read -r line; do
 
+	echo "" | tee -a  ${domain}/secrets.txt
+	echo "----- $line -----" | tee -a ${domain}/secrets.txt
+	python3 ~/tools/SecretFinder/SecretFinder.py -i $line -o cli | tee -a ${domain}/secrets.txt
 
-## Todo
-# Get subdomains //
-# Get robots.txt 
-# Get Directories
-# Get Screenshots //
-# Get IP addresses, ranges, CIDR, hostnames
-# Get live subdomains
-# Get waybackurls
-# Get vulnerable endpoints
-# Get JS code
-# Get links
-# Get company employees
-# Get company repo names / count?
-# Internal domains via whoxy.com -> get subs -> JS -> info leak
+done < $domain/live_subdomains.txt
+
+mkdir ${domain}/files
+
+echo ""
+echo $'\e[32m'"Finding JS Files..." $'\e[0m'
+grep -i ".js$" ${domain}/links.txt | sort -u >> ${domain}/files/js.txt
+
+echo ""
+echo $'\e[32m'"Finding PHP Files..." $'\e[0m'
+grep -i ".php$" ${domain}/links.txt | sort -u >> ${domain}/files/php.txt
+
+echo ""
+echo $'\e[32m'"Finding HTML Files..." $'\e[0m'
+grep -i ".html$" ${domain}/links.txt | sort -u >> ${domain}/files/html.txt
+
+echo ""
+echo $'\e[32m'"Finding CSS Files..." $'\e[0m'
+grep -i ".css$" ${domain}/links.txt | sort -u >> ${domain}/files/css.txt
+
+echo ""
+echo $'\e[32m'"Finding Images..." $'\e[0m'
+grep -iE ".png$|.jpg$|.svg$|.gif$" ${domain}/links.txt | sort -u >> ${domain}/files/images.txt
+#
+# # robots //
+# # all links
+# # all secrets
+# # Organise links by file type
+# # download and beautify js files
+#
+#
+#
+# ## Todo
+# # Get subdomains //
+# # Get robots.txt //
+# # Get Directories
+# # Get Screenshots //
+# # Get IP addresses, ranges, CIDR, hostnames
+# # Get live subdomains
+# # Get waybackurls
+# # Get vulnerable endpoints
+# # Get JS code
+# # Get links
+# # Get company employees
+# # Get company repo names / count?
+# # Internal domains via whoxy.com -> get subs -> JS -> info leak
